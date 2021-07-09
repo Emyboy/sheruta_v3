2a4602ed8bcda8f9677a4b67d73bcd5abcd52bff
emykumba <emy@meetkumba.com>
2021-07-09 01:29:58 +0100

    ...

--- src/components/EachRequest/EachRequest.jsx
+++ src/components/EachRequest/EachRequest.jsx
@@ -6,15 +6,15 @@ import { Tag } from 'antd';
 export default function EachRequest({
     data
 }) {
+    console.log(data)
     return (
-
         <article className='bg-white p-2 rounded border mb-3 '>
-            <div className="comment-details">
+            <div className="comment-details pl-3">
                 {
                     data.users_permissions_user ?
                         <div className="comment-meta row">
-                            <div className="article_comments_thumb">
-                                <Avatar src={data.users_permissions_user.avatar_url} size={50} className='mr-2' />
+                            <div className="article_comments_thumb" style={{ width: '60px' }}>
+                                <Avatar src={data.users_permissions_user.avatar_url} size={50}/>
                             </div>
                             <div className="comment-left-meta">
                                 <h4 className="author-name mb-1" style={{ fontSize: '20px' }}>{data.users_permissions_user.first_name}</h4>
@@ -35,7 +35,16 @@ export default function EachRequest({
                 <div className="comment-text mt-1">
                     <p>{data.body.length > 90 ? data.body.slice(0, 90) + "..." : data.body}</p>
                 </div>
+                <hr className='mt-1 mb-1' />
+                {
+                    data.users_permissions_user ?
+                        <a href={`tel:${data.users_permissions_user.phone_number}`}>
+                            <span className='badge badge-danger' style={{ fontSize: '15px' }}><i className='ti-mobile'></i> Call Me</span>
+                        </a>
+                        : null
+                }
             </div>
+
         </article>
 
     )
