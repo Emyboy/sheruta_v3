3e87acc047b66137370a53cd430189124c5a94ce
emykumba <emy@meetkumba.com>
2021-07-11 13:57:54 +0100

    ..

--- src/components/Popups/PaymentPopup.jsx
+++ src/components/Popups/PaymentPopup.jsx
@@ -7,26 +7,42 @@ import { Link } from 'react-router-dom';
 
 const PaymentPopup = (props) => {
     const { view } = props;
-    return (
-        <Modal show={view.showPaymentPopup} style={{ marginTop: '20vh' }} className='animated animate__bounceIn'>
-            <Modal.Body>
-                <div className='text-center'>
-                    <i className='ti-alert display-3 text-warning'></i>
-                    <h4><b>No or invalid subscription plan</b></h4>
-                </div>
-                <hr className='text-muted' />
-                <label><b>Why you are seeing this.</b></label>
-                <ol>
-                    <li>Your subscription may have expired</li>
-                    <li>Your subscription type doesn't enable you perform this action</li>
-                </ol>
-                <span className='text-danger'><b>NOTE: </b></span><span>If this is a mistake, don't fail to contact us</span>
-                <hr className='text-muted' />
-               <div className='text-center'>
-                    <Link to='/pricing'>
+    if (view.showPaymentPopup) {
+
+        return (
+            <Modal show={view.showPaymentPopup} className='animated animate__bounceIn'>
+                <Modal.Body>
+                    <div className='text-center'>
+                        <i className='ti-alert display-3 text-warning'></i>
+                        <h4><b>No or invalid subscription plan</b></h4>
+                    </div>
+                    <hr className='text-muted' />
+                    <label><b>Why you are seeing this.</b></label>
+                    <ol>
+                        <li>Your subscription may have expired</li>
+                        <li>Your subscription type doesn't enable you perform this action</li>
+                    </ol>
+                    <span className='text-danger'><b>NOTE: </b></span><span>If this is a mistake, don't fail to contact us</span>
+                    <hr className='text-muted' />
+                    <div className='text-center'>
+                        <Link to='/pricing'>
+                            <Btn
+                                text='Subscribe Now'
+                                className='btn-sm'
+                                onClick={() => {
+                                    store.dispatch({
+                                        type: 'SET_VIEW_STATE',
+                                        payload: {
+                                            showPaymentPopup: false
+                                        }
+                                    })
+                                }}
+                            />
+                        </Link>
+                        <br />
                         <Btn
-                            text='Subscribe Now'
-                            className='btn-sm'
+                            text='Close'
+                            className='btn-sm mt-4 bg-light text-danger'
                             onClick={() => {
                                 store.dispatch({
                                     type: 'SET_VIEW_STATE',
@@ -36,24 +52,12 @@ const PaymentPopup = (props) => {
                                 })
                             }}
                         />
-                    </Link>
-                    <br />
-                    <Btn
-                        text='Close'
-                        className='btn-sm mt-4 bg-light text-danger'
-                        onClick={() => {
-                            store.dispatch({
-                                type: 'SET_VIEW_STATE',
-                                payload: {
-                                    showPaymentPopup: false
-                                }
-                            })
-                        }}
-                    />
-               </div>
-            </Modal.Body>
-        </Modal>
-    )
+                    </div>
+                </Modal.Body>
+            </Modal>
+        )
+    } else
+        return null
 }
 
 const mapStateToProps = (state) => ({
