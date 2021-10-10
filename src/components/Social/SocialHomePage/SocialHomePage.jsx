46a53395d7b33d67f5140a0aa9f6e0c615e5a5dc
emykumba <emy@meetkumba.com>
2021-10-10 03:37:29 +0100

    ...

--- src/components/Social/SocialHomePage/SocialHomePage.jsx
+++ src/components/Social/SocialHomePage/SocialHomePage.jsx
@@ -19,7 +19,7 @@ export default (props) => {
   const { user } = auth.user;
   const [state, setState] = useState({
     properties: [],
-    // list: [],
+    list: [],
   });
   const [newUsers, setNewUsers] = useState([]);
 
@@ -54,14 +54,14 @@ export default (props) => {
       .catch((err) => {});
   }, []);
   useEffect(() => {
-    if (view.feed.length === 0) {
+    if (state.list.length === 0) {
       axios(
         process.env.REACT_APP_API_URL +
           `/property-requests/?is_searching=${!view.personal_info
             .looking_for}&_limit=25&_start=0&_sort=created_at:DESC`
       )
         .then((res) => {
-          // setState({ ...state, list: res.data });
+          setState({ ...state, list: res.data });
           console.log('FEED -----', res.data)
           dispatch({
               type: "SET_VIEW_STATE",
@@ -434,7 +434,7 @@ export default (props) => {
                                   </div>
                               )}
                               <div className="col-lg-5">
-                                  {view.feed.map((val, i) => {
+                                  {state.list.map((val, i) => {
                                       return (
                                           <EachSocialRequest
                                               key={i} 
@@ -442,7 +442,7 @@ export default (props) => {
                                           />
                                       );
                                   })}
-                                  {view.feed.length === 0 ? (
+                                  {state.list.length === 0 ? (
                                       <div className="central-meta item rounded border-gray text-center d-flex justify-content-center mt-5 pt-5">
                                           <Spinner />
                                       </div>
