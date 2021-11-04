2b6c798a8b9ab20f171e68e73e2eb8ea39bf4274
emykumba <emy@meetkumba.com>
2021-11-04 23:49:13 +0100

    removed notifications

--- src/components/Popups/MasterPopup.js
+++ src/components/Popups/MasterPopup.js
@@ -52,7 +52,7 @@ const MasterPopup = (props) => {
             <>
                 <ConfigViewPopup />
                 <GetStartedPopup />
-                <NotificationPopup />
+                {/* <NotificationPopup /> */}
                 <AppUpdatePopup />
             </>
         );
