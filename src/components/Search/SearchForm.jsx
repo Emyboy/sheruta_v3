872f515ca0ea34f72e995cc561a476713e21e77c
emykumba <emy@meetkumba.com>
2021-12-01 04:58:22 +0100

    changed settings

--- src/components/Search/SearchForm.jsx
+++ src/components/Search/SearchForm.jsx
@@ -1,566 +1,94 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 
 export default function SearchForm() {
+	const { services, categories } = useSelector(state => state.view);
 	return (
 		<div className="col-lg-12 mt-4">
 			<div className="card w-100 p-4 border-0 bg-lightblue">
 				<div className="row">
 					<div className="col-lg-3">
 						<div className="form-group mb-0">
-							<label htmlFor="Search" className="fw-600 text-grey-900 font-xsss">
-								Categories
-							</label>
-						</div>
-						<ul className="recent-post mt-2">
-							<li className="mb-0">
-								<a href="#" className="fw-600 font-xssss">
-									Drress
-								</a>
-							</li>
-							<li className="mb-0">
-								<a href="#" className="fw-600 font-xssss">
-									Shirt
-								</a>
-							</li>
-							<li className="mb-0">
-								<a href="#" className="fw-600 font-xssss">
-									Jacket
-								</a>
-							</li>
-							<li className="mb-0">
-								<a href="#" className="fw-600 font-xssss">
-									Jean
-								</a>
-							</li>
-							<li className="mb-0">
-								<a href="#" className="fw-600 font-xssss">
-									Men
-								</a>
-							</li>
-							<li className="mb-0">
-								<a href="#" className="fw-600 font-xssss">
-									Shorts
-								</a>
-							</li>
-							<li className="mb-0">
-								<a href="#" className="fw-600 font-xssss">
-									Swimwear
-								</a>
-							</li>
-							<li className="mb-0">
-								<a href="#" className="fw-600 font-xssss">
-									Tousers
-								</a>
-							</li>
-							<li className="mb-0">
-								<a href="#" className="fw-600 font-xssss">
-									Jumper Cardigans
-								</a>
-							</li>
-						</ul>
-					</div>
-					<div className="col-lg-3 pe-4">
-						<div className="form-group mb-0">
-							<label htmlFor="Search" className="fw-600 text-grey-900 font-xsss">
-								Brand
-							</label>
-						</div>
-						<div className="form-group mb-2 mt-2">
-							<div className="custom-control me-0 custom-radio size-sm custom-control-inline d-block">
-								<input
-									type="radio"
-									className="custom-control-input"
-									id="customRadio1"
-									name="Depart"
-									value="customEx"
-									checked=""
-								/>
-								<label
-									className="custom-control-label small-size fw-600 text-grey-800 font-xssss d-block"
-									htmlFor="customRadio1"
-								>
-									Addidas
-									<span className="float-right fw-600 text-grey-600 font-xssss mt-1">
-										12
-									</span>
-								</label>
-							</div>
-						</div>
-						<div className="form-group mb-2">
-							<div className="custom-control me-0 custom-radio size-sm custom-control-inline d-block">
-								<input
-									type="radio"
-									className="custom-control-input"
-									id="customRadio5"
-									name="Depart"
-									value="customEx"
-								/>
-								<label
-									className="custom-control-label small-size fw-600 text-grey-800 font-xssss d-block"
-									htmlFor="customRadio5"
-								>
-									Levis{' '}
-									<span className="float-right fw-600 text-grey-600 font-xssss mt-1">
-										15
-									</span>
-								</label>
-							</div>
-						</div>
-						<div className="form-group mb-2">
-							<div className="custom-control me-0 custom-radio size-sm custom-control-inline d-block">
-								<input
-									type="radio"
-									className="custom-control-input"
-									id="customRadio3"
-									name="Depart"
-									value="customEx"
-								/>
-								<label
-									className="custom-control-label small-size fw-600 text-grey-800 font-xssss d-block"
-									htmlFor="customRadio3"
-								>
-									Cape Town{' '}
-									<span className="float-right fw-600 text-grey-600 font-xssss mt-1">
-										7
-									</span>
-								</label>
-							</div>
-						</div>
-						<div className="form-group mb-2">
-							<div className="custom-control me-0 custom-radio size-sm custom-control-inline d-block">
-								<input
-									type="radio"
-									className="custom-control-input"
-									id="customRadio4"
-									name="Depart"
-									value="customEx"
-								/>
-								<label
-									className="custom-control-label small-size fw-600 text-grey-800 font-xssss d-block"
-									htmlFor="customRadio4"
-								>
-									Nikes{' '}
-									<span className="float-right fw-600 text-grey-600 font-xssss mt-1">
-										34
-									</span>
-								</label>
-							</div>
-						</div>
-						<div className="form-group mb-2 mt-2">
-							<div className="custom-control me-0 custom-radio size-sm custom-control-inline d-block">
-								<input
-									type="radio"
-									className="custom-control-input"
-									id="customRadio1"
-									name="Depart"
-									value="customEx"
-									checked=""
-								/>
-								<label
-									className="custom-control-label small-size fw-600 text-grey-800 font-xssss d-block"
-									htmlFor="customRadio1"
-								>
-									Addidas
-									<span className="float-right fw-600 text-grey-600 font-xssss mt-1">
-										12
-									</span>
-								</label>
-							</div>
-						</div>
-						<div className="form-group mb-2">
-							<div className="custom-control me-0 custom-radio size-sm custom-control-inline d-block">
-								<input
-									type="radio"
-									className="custom-control-input"
-									id="customRadio5"
-									name="Depart"
-									value="customEx"
-								/>
-								<label
-									className="custom-control-label small-size fw-600 text-grey-800 font-xssss d-block"
-									htmlFor="customRadio5"
-								>
-									Levis{' '}
-									<span className="float-right fw-600 text-grey-600 font-xssss mt-1">
-										15
-									</span>
-								</label>
-							</div>
-						</div>
-						<div className="form-group mb-2">
-							<div className="custom-control me-0 custom-radio size-sm custom-control-inline d-block">
-								<input
-									type="radio"
-									className="custom-control-input"
-									id="customRadio3"
-									name="Depart"
-									value="customEx"
-								/>
-								<label
-									className="custom-control-label small-size fw-600 text-grey-800 font-xssss d-block"
-									htmlFor="customRadio3"
-								>
-									Cape Town{' '}
-									<span className="float-right fw-600 text-grey-600 font-xssss mt-1">
-										7
-									</span>
-								</label>
-							</div>
-						</div>
-						<div className="form-group mb-2">
-							<div className="custom-control me-0 custom-radio size-sm custom-control-inline d-block">
-								<input
-									type="radio"
-									className="custom-control-input"
-									id="customRadio4"
-									name="Depart"
-									value="customEx"
-								/>
-								<label
-									className="custom-control-label small-size fw-600 text-grey-800 font-xssss d-block"
-									htmlFor="customRadio4"
-								>
-									Nikes{' '}
-									<span className="float-right fw-600 text-grey-600 font-xssss mt-1">
-										34
-									</span>
-								</label>
-							</div>
-						</div>
-					</div>
-					<div className="col-lg-3">
-						<div className="form-group mb-0">
-							<label htmlFor="Search" className="fw-600 text-grey-900 font-xsss">
-								Size
+							<label
+								htmlFor="Search"
+								className="fw-600 text-grey-900 font-xsss"
+							>
+								Bedrooms
 							</label>
 						</div>
 						<ul className="list-inline mt-2">
-							<li className="list-inline-item me-0 mb-2">
-								<a
-									href="#"
-									className="btn-round-sm bg-white fw-600 font-xssss text-grey-800 "
-								>
-									XS
-								</a>
-							</li>
-							<li className="list-inline-item me-0 mb-2">
-								<a
-									href="#"
-									className="btn-round-sm bg-white fw-600 font-xssss text-grey-800 "
-								>
-									S
-								</a>
-							</li>
-							<li className="list-inline-item me-0 mb-2">
-								<a
-									href="#"
-									className="btn-round-sm bg-white fw-600 font-xssss text-grey-800 "
-								>
-									SM
-								</a>
-							</li>
-							<li className="list-inline-item me-0 mb-2">
-								<a
-									href="#"
-									className="btn-round-sm bg-white fw-600 font-xssss text-grey-800 "
-								>
-									M
-								</a>
-							</li>
-							<li className="list-inline-item me-0 mb-2">
-								<a
-									href="#"
-									className="btn-round-sm bg-white fw-600 font-xssss text-grey-800 "
-								>
-									L
-								</a>
-							</li>
-							<li className="list-inline-item me-0 mb-2">
-								<a
-									href="#"
-									className="btn-round-sm bg-white fw-600 font-xssss text-grey-800 "
-								>
-									XL
-								</a>
-							</li>
-							<li className="list-inline-item me-0 mb-2">
-								<a
-									href="#"
-									className="btn-round-sm bg-white fw-600 font-xssss text-grey-800 "
-								>
-									XS
-								</a>
-							</li>
-							<li className="list-inline-item me-0 mb-2">
-								<a
-									href="#"
-									className="btn-round-sm bg-white fw-600 font-xssss text-grey-800 "
-								>
-									S
-								</a>
-							</li>
-							<li className="list-inline-item me-0 mb-2">
-								<a
-									href="#"
-									className="btn-round-sm bg-white fw-600 font-xssss text-grey-800 "
-								>
-									SM
-								</a>
-							</li>
-							<li className="list-inline-item me-0 mb-2">
-								<a
-									href="#"
-									className="btn-round-sm bg-white fw-600 font-xssss text-grey-800 "
-								>
-									M
-								</a>
-							</li>
-							<li className="list-inline-item me-0 mb-2">
-								<a
-									href="#"
-									className="btn-round-sm bg-white fw-600 font-xssss text-grey-800 "
-								>
-									L
-								</a>
-							</li>
-							<li className="list-inline-item me-0 mb-2">
-								<a
-									href="#"
-									className="btn-round-sm bg-white fw-600 font-xssss text-grey-800 "
-								>
-									XL
-								</a>
-							</li>
-						</ul>
-
-						<div className="form-group mb-0 mt-4">
-							<label htmlFor="Search" className="fw-600 text-grey-900 font-xsss">
-								Color
-							</label>
-						</div>
-						<ul className="list-inline mt-2">
-							<li className="list-inline-item me-0">
-								<a
-									href="#"
-									className="btn-round-sm"
-									// style="background-color: #ff3b30;"
-								></a>
-							</li>
-							<li className="list-inline-item me-0">
-								<a
-									href="#"
-									className="btn-round-sm"
-									// style="background-color: #4cd964;"
-								></a>
-							</li>
-							<li className="list-inline-item me-0">
-								<a
-									href="#"
-									className="btn-round-sm"
-									// style="background-color: #132977;"
-								></a>
-							</li>
-							<li className="list-inline-item me-0">
-								<a
-									href="#"
-									className="btn-round-sm"
-									// style="background-color: #ff2d55;"
-								></a>
-							</li>
-							<li className="list-inline-item me-0">
-								<a
-									href="#"
-									className="btn-round-sm"
-									// style="background-color: #ffcc00;"
-								></a>
-							</li>
-							<li className="list-inline-item me-0">
-								<a
-									href="#"
-									className="btn-round-sm"
-									// style="background-color: #ff9500;"
-								></a>
-							</li>
-							<li className="list-inline-item me-0">
-								<a
-									href="#"
-									className="btn-round-sm"
-									// style="background-color: #8e8e93;"
-								></a>
-							</li>
-							<li className="list-inline-item me-0">
-								<a
-									href="#"
-									className="btn-round-sm"
-									// style="background-color: #D2691E;"
-								></a>
-							</li>
-							<li className="list-inline-item me-0">
-								<a
-									href="#"
-									className="btn-round-sm"
-									// style="background-color: #228B22;"
-								></a>
-							</li>
-							<li className="list-inline-item me-0">
-								<a
-									href="#"
-									className="btn-round-sm"
-									// style="background-color: #FFC0CB;"
-								></a>
-							</li>
-							<li className="list-inline-item me-0">
-								<a
-									href="#"
-									className="btn-round-sm"
-									// style="background-color: #5f9ea0;"
-								></a>
-							</li>
-							<li className="list-inline-item me-0">
-								<a
-									href="#"
-									className="btn-round-sm"
-									// style="background-color: #9932cc;"
-								></a>
-							</li>
+							{[1, 2, 3, 4].map((val, i) => {
+								return (
+									<li className="list-inline-item me-0 mb-2" key={`${i}-bed`}>
+										<a
+											href="#"
+											className="btn-round-sm bg-white fw-600 font-xssss text-grey-800 "
+										>
+											{val}
+										</a>
+									</li>
+								)
+							})}
 						</ul>
 					</div>
 					<div className="col-lg-3">
-						<div className="form-group mb-0 mt-0">
-							<label htmlFor="Search" className="fw-600 text-grey-900 font-xsss">
-								Price
-							</label>
-						</div>
-						<div className="form-group mt-3 mb-0">
-							<div className="row">
-								<div className="col-sm-12">
-									<div
-										id="slider-range"
-										className="mb-2 noUi-target noUi-ltr noUi-horizontal noUi-background"
-									>
-										<div className="noUi-base">
-											<div
-												className="noUi-origin noUi-connect"
-												style={{ left: '9.09091%' }}
-											>
-												<div className="noUi-handle noUi-handle-lower"></div>
-											</div>
-											<div
-												className="noUi-origin noUi-background"
-												style={{ left: '79.798%' }}
-											>
-												<div className="noUi-handle noUi-handle-upper"></div>
-											</div>
-										</div>
-									</div>
-								</div>
-							</div>
-							<div className="row slider-labels">
-								<div className="col-6 caption">
-									<span
-										id="slider-range-value1"
-										className="font-xsssss fw-600 text-grey-600"
-									>
-										$1,000
-									</span>
-								</div>
-								<div className="col-6 text-right caption">
-									<span
-										id="slider-range-value2"
-										className="font-xsssss fw-600 text-grey-600"
-									>
-										$8,000
-									</span>
-								</div>
-							</div>
-							<div className="row">
-								<div className="col-sm-12">
-									<form>
-										<input type="hidden" name="min-value" value="" />
-										<input type="hidden" name="max-value" value="" />
-									</form>
-								</div>
-							</div>
-						</div>
 						<div className="form-group mb-0 mt-3">
-							<label htmlFor="Search" className="fw-600 text-grey-900 font-xsss">
-								Tag
+							<label
+								htmlFor="Search"
+								className="fw-600 text-grey-900 font-xsss"
+							>
+								Service
 							</label>
 						</div>
 						<ul className="list-inline mt-2">
-							<li className="m-0 list-inline-item mb-2">
-								<a
-									href="#"
-									className="fw-600 font-xssss text-grey-700 pt-1 pb-1 ps-3 pe-3 d-inline-block rounded-xl bg-white"
-								>
-									Drress
-								</a>
-							</li>
-							<li className="m-0 list-inline-item mb-2">
-								<a
-									href="#"
-									className="fw-600 font-xssss text-grey-700 pt-1 pb-1 ps-3 pe-3 d-inline-block rounded-xl bg-white"
-								>
-									Shirt
-								</a>
-							</li>
-							<li className="m-0 list-inline-item mb-2">
-								<a
-									href="#"
-									className="fw-600 font-xssss text-grey-700 pt-1 pb-1 ps-3 pe-3 d-inline-block rounded-xl bg-white"
-								>
-									Jacket
-								</a>
-							</li>
-							<li className="m-0 list-inline-item mb-2">
-								<a
-									href="#"
-									className="fw-600 font-xssss text-grey-700 pt-1 pb-1 ps-3 pe-3 d-inline-block rounded-xl bg-white"
-								>
-									Jean
-								</a>
-							</li>
-							<li className="m-0 list-inline-item mb-2">
-								<a
-									href="#"
-									className="fw-600 font-xssss text-grey-700 pt-1 pb-1 ps-3 pe-3 d-inline-block rounded-xl bg-white"
-								>
-									Men
-								</a>
-							</li>
-							<li className="m-0 list-inline-item mb-2">
-								<a
-									href="#"
-									className="fw-600 font-xssss text-grey-700 pt-1 pb-1 ps-3 pe-3 d-inline-block rounded-xl bg-white"
-								>
-									Shorts
-								</a>
-							</li>
-							<li className="m-0 list-inline-item mb-2">
-								<a
-									href="#"
-									className="fw-600 font-xssss text-grey-700 pt-1 pb-1 ps-3 pe-3 d-inline-block rounded-xl bg-white"
-								>
-									Swimwear
-								</a>
-							</li>
-							<li className="m-0 list-inline-item mb-2">
-								<a
-									href="#"
-									className="fw-600 font-xssss text-grey-700 pt-1 pb-1 ps-3 pe-3 d-inline-block rounded-xl bg-white"
-								>
-									Tousers
-								</a>
-							</li>
-							<li className="m-0 list-inline-item mb-2">
-								<a
-									href="#"
-									className="fw-600 font-xssss text-grey-700 pt-1 pb-1 ps-3 pe-3 d-inline-block rounded-xl bg-white"
-								>
-									Jumper Cardigans
-								</a>
-							</li>
+							{services.map((service, i) => {
+								return (
+									<li
+										className="m-0 list-inline-item mb-2"
+										key={`service-${i}`}
+									>
+										<a
+											href="#"
+											className="fw-600 font-xssss text-grey-700 pt-1 pb-1 ps-3 pe-3 d-inline-block rounded-xl bg-white"
+										>
+											{service.name}
+										</a>
+									</li>
+								)
+							})}
 						</ul>
+					</div>
+					<div className="col-lg-3">
+						<div className="form-group mb-0 mt-3">
+							<label
+								htmlFor="Search"
+								className="fw-600 text-grey-900 font-xsss"
+							>
+								Type
+							</label>
+						</div>
+						<ul className="list-inline mt-2">
+							{categories.map((service, i) => {
+								return (
+									<li
+										className="m-0 list-inline-item mb-2"
+										key={`service-${i}`}
+									>
+										<a
+											href="#"
+											className="fw-600 font-xssss text-grey-700 pt-1 pb-1 ps-3 pe-3 d-inline-block rounded-xl bg-white"
+										>
+											{service.name}
+										</a>
+									</li>
+								)
+							})}
+						</ul>
+						<button
+							class="mt-4 p-0 btn p-2 lh-24 w100 ms-1 ls-3 d-inline-block rounded-xl bg-current  fw-700 ls-lg text-white w-50"
+						>
+							Search
+						</button>
 					</div>
 				</div>
 			</div>
