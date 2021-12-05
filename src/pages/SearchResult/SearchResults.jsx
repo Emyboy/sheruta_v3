fe20732f915ac9e82b601459d02e28bf1cc3f720
emykumba <emy@meetkumba.com>
2021-12-05 04:16:32 +0100

    added deactivation

--- src/pages/SearchResult/SearchResults.jsx
+++ src/pages/SearchResult/SearchResults.jsx
@@ -1,12 +1,42 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
 import Layout from '../../components/Layout/Layout';
+import EachSocialRequest from '../../components/Social/EachSocialRequest'
 
-export default function SearchResults() {
-    return (
-			<Layout>
-				<div>
-					<h1>Search results</h1>
-				</div>
-			</Layout>
+export default function SearchResults({ match }) {
+	const { bedrooms, category, service } = match.params
+	console.log({ bedrooms, category, service });
+	const [list, setList] = useState([])
+
+	useEffect(() => {
+		axios(
+			process.env.REACT_APP_API_URL +
+				`/property-requests/search/${service}/${category}/${bedrooms || 0}`,
+			{ method: 'POST' }
 		)
+			.then(res => {
+				console.log(res.data);
+				setList(res.data)
+			})
+			.catch(err => {
+				console.log(err)
+			})
+	}, [])
+
+	return (
+		<Layout>
+			<div>
+				<h1>Search results</h1>
+				<div className='container'>
+					<div className='row justify-content-center'>
+						{
+							list.map((val,i) => {
+								return <EachSocialRequest key={`req-${i}`} data={val} />
+							})
+						}
+					</div>
+				</div>
+			</div>
+		</Layout>
+	)
 }
