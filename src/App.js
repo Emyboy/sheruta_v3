d2ff733a7843b37975a104310f08fcdd28624b24
emykumba <emy@meetkumba.com>
2021-12-06 02:34:08 +0100

    construction

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import UnderConstruction from './components/UnderConstruction';
 import Router from './Router'
 
 function App() {
 
   return (
     <div className="App">
-      <Router />
+      {/* <Router /> */}
+      <UnderConstruction />
     </div>
   );
 }
