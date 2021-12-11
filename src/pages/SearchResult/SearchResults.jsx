ec6802baed041799b0d28f66d93762414b6b52d1
emykumba <emy@meetkumba.com>
2021-12-11 20:17:35 +0100

    fixed issues

--- src/pages/SearchResult/SearchResults.jsx
+++ src/pages/SearchResult/SearchResults.jsx
@@ -32,24 +32,30 @@ export default function SearchResults({ match }) {
 			<div>
 				<div className="container h-100">
 					<div className="row justify-content-center">
-						<div className="col-md-8 col-sm-12">
+						<div className="col-md-9 col-sm-12">
 							<div className="card rounded mb-4 border-0 shadow-sm">
 								<div className="card-body">
 									<h1>Search results</h1>
 								</div>
 							</div>
-							{loading ?
-							(
+							{loading ? (
 								<div className="text-center pt-5 pb-5">
 									<Dots />
 								</div>
-							):null}
-							{ !loading && list.length > 0 ? list.map((val, i) => {
-								return <EachSocialRequest key={`req-${i}`} data={val} />
-							}): <div className='pt-5 pb-5 text-center'>
-									<h2>No Results Found</h2>
+							) : null}
+							{!loading &&
+								list.length > 0 &&
+								list.map((val, i) => {
+									return <EachSocialRequest key={`req-${i}`} data={val} />
+								})}
+							{!loading && list.length === 0 && (
+								<div className="pt-5 pb-5 text-center">
+									<h2>
+										<b>No Results Found</b>
+									</h2>
 									<h6>Try narrowing your search down to one thing</h6>
-								</div>}
+								</div>
+							)}
 						</div>
 					</div>
 				</div>
