ed2869b8feab9a9797def7042108011dacbc175f
emykumba <emy@meetkumba.com>
2021-12-11 15:55:57 +0100

    ..

--- src/components/Ads/RequestAds/PostRequestAds.jsx
+++ src/components/Ads/RequestAds/PostRequestAds.jsx
@@ -6,31 +6,29 @@ import Btn from "../../Btn/Btn";
 export default function PostRequestAds() {
     const { view } = useSelector((state) => state);
     return (
-        <>
-            {view.personal_info && (
-                <div
-                    className="card rounded border-gray mb-3 shadow"
-                    style={{
-                        backgroundColor: "#202323",
-                    }}
-                >
-                    <div className="card-body">
-                        <h3 className="text-white">
-                            {view.personal_info.is_looking_for
-                                ? "Looking for a flat to share?"
-                                : "Have a flat to share?"}
-                        </h3>
-                        <p className="text-white">Post a request today. 🚀</p>
-                        <Link to="/requests/create">
-                            <Btn
-                                text="Post Now"
-                                className="btn-sm"
-                                onClick={() => {}}
-                            />
-                        </Link>
-                    </div>
-                </div>
-            )}
-        </>
-    );
+			<>
+				{view.personal_info && (
+					<div
+						className="card rounded border-gray mb-3 shadow"
+						style={{
+							backgroundColor: '#202323',
+						}}
+					>
+						<div className="card-body">
+							<h1 className="text-white">
+								<b>
+									{view.personal_info.is_looking_for
+										? 'Looking for a flat to share?'
+										: 'Have a flat to share?'}
+								</b>
+							</h1>
+							<p className="text-white">Post a request today. 🚀</p>
+							<Link to="/requests/create">
+								<Btn text="Post Now" className="btn-sm" onClick={() => {}} />
+							</Link>
+						</div>
+					</div>
+				)}
+			</>
+		)
 }
