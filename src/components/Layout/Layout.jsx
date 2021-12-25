1659a88483d1d8b9c042b81aff8c77ca7b733e1e
emykumba <emy@meetkumba.com>
2021-12-25 23:15:21 +0100

    ..

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -28,7 +28,7 @@ export default function Layout({ currentPage, children, showMessages }) {
 
 			<div
 				className={user && 'main-content right-chat-active'}
-				style={{ paddingBottom: !user ? '0vh' : '21vh', height: '100vh' }}
+				style={{ paddingBottom: !user ? '0vh' : '21vh' }}
 			>
 				<div className={user && 'middle-sidebar-bottom pl-0 pr-0'}>
 					<div className={user && 'middle-sidebar-left'}>{children}</div>
