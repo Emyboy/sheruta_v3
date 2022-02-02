998a7cb3affe830eccdbe0373d697a3a230d1928
emykumba <emy@meetkumba.com>
2022-02-02 01:21:23 +0100

    ..

--- src/utils/request.utils.js
+++ src/utils/request.utils.js
@@ -4,7 +4,7 @@ module.exports = {
 			request?.is_searching
 				? `Looking for ${request?.category ? request?.category?.name : ''} in`
 				: `${request?.category ? request?.category?.name : ''} for share in`
-		} ${request?.location}`?.replace(/\s/g, '-')?.toLocaleLowerCase()
+		} ${request?.location}`?.replace(/\s/g, '-')?.toLocaleLowerCase().replace(`/`,'-')
 			
 		return `/request/${title}/${request?.id}`
 	},
