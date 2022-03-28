36e760880776a30a350a9ee4a5bd5d5283e38fb5
emykumba <emy@meetkumba.com>
2022-03-29 00:41:03 +0100

    added delete and edit for reviews

--- src/components/ReviewForm/ReviewForm.jsx
+++ src/components/ReviewForm/ReviewForm.jsx
@@ -1,50 +1,90 @@
 import React, { useState } from 'react'
 import { useSelector } from 'react-redux'
-import ReactStars from 'react-rating-stars-component';
-import { FaStar } from 'react-icons/fa';
-import { AiOutlineStar } from 'react-icons/ai';
-import axios from 'axios';
-import Cookies from 'js-cookie';
+import ReactStars from 'react-rating-stars-component'
+import { FaStar } from 'react-icons/fa'
+import { AiOutlineStar } from 'react-icons/ai'
+import axios from 'axios'
+import Cookies from 'js-cookie'
+import { notification } from 'antd'
 
-export default function ReviewForm({ heading, done, request }) {
-	const { user } = useSelector((state) => state.auth);
-    const [review, setReview] = useState(null);
-    const [rating, setRating] = useState(0);
-    const [loading, setLoading] = useState(false);
+export default function ReviewForm({
+	heading,
+	done,
+	request,
+	edit,
+	reviewData,
+}) {
+	const { user } = useSelector((state) => state.auth)
+	const [review, setReview] = useState(reviewData ? reviewData?.review : null)
+	const [rating, setRating] = useState(reviewData ? reviewData?.rating : 1)
+	const [loading, setLoading] = useState(false)
 
-    const handleSubmit = async (e) => {
-        e.preventDefault()
-        try {
-            setLoading(true)
-            const res = await axios(process.env.REACT_APP_API_URL+`/reviews`, {
-                method: 'POST',
-                headers: {
-                    Authorization: `Bearer ${Cookies.get('token')}`
-                },
-                data: {
-                    review,
-                    user: user?.user?.id,
-                    rating,
-                    request
-                }
-            });
-            if(res?.data){
-                done(res.data)
-                setLoading(false);
-                setReview('')
-                setRating(0)
-            }
-        } catch (error) {
-            setLoading(false)
-            return Promise.reject(error)
-        }
-    }
+	const updateReviews = async (e) => {
+		e.preventDefault()
+		try {
+			setLoading(true)
+			const res = await axios(
+				process.env.REACT_APP_API_URL + `/reviews/${reviewData?.id}`,
+				{
+					method: 'PUT',
+					headers: {
+						Authorization: `Bearer ${Cookies.get('token')}`,
+					},
+					data: {
+						review,
+						rating,
+					},
+				}
+			)
+			if (res?.data) {
+				if(done){
+					done(res.data)
+				}
+				setLoading(false)
+				notification.success({ message: 'Saved' })
+			}
+		} catch (error) {
+			notification.error({ message: 'Error, please try again' })
+			setLoading(false)
+			return Promise.reject(error)
+		}
+	}
+
+	const handleSubmit = async (e) => {
+		e.preventDefault()
+		try {
+			setLoading(true)
+			const res = await axios(process.env.REACT_APP_API_URL + `/reviews`, {
+				method: 'POST',
+				headers: {
+					Authorization: `Bearer ${Cookies.get('token')}`,
+				},
+				data: {
+					review,
+					user: user?.user?.id,
+					rating,
+					request,
+				},
+			})
+			if (res?.data) {
+				if(done){
+					done(res.data)
+				}
+				setLoading(false)
+				setReview('')
+				setRating(1)
+			}
+		} catch (error) {
+			setLoading(false)
+			return Promise.reject(error)
+		}
+	}
 
 	if (!user) {
 		return null
 	}
 	return (
-		<form onSubmit={handleSubmit}>
+		<form onSubmit={edit ? updateReviews : handleSubmit}>
 			<div className="card w-100 shadow-xss rounded-xxl border-0 ps-4 pt-4 pe-4 pb-3 mb-3 mt-3">
 				<div className="card-body p-0">
 					<a className=" font-xssss fw-600 text-grey-500 card-body p-0 d-flex align-items-center">
@@ -58,7 +98,7 @@ export default function ReviewForm({ heading, done, request }) {
 						onChange={(e) => {
 							setRating(e)
 						}}
-                        value={rating}
+						value={rating}
 						emptyIcon={<AiOutlineStar />}
 						fullIcon={<FaStar />}
 						size={24}
@@ -79,16 +119,16 @@ export default function ReviewForm({ heading, done, request }) {
 						cols="30"
 						rows="10"
 						placeholder="What's on your mind?"
-                        value={review}
-                        onChange={e => setReview(e.target.value)}
-                        maxLength={"240"}
+						value={review}
+						onChange={(e) => setReview(e.target.value)}
+						maxLength={'240'}
 					></textarea>
 				</div>
 				<button
-                    disabled={ (rating === 0) || loading}
+					disabled={rating === 0 || loading}
 					className="btn text-center p-2 lh-24 w100 ms-1 ls-3 d-inline-block rounded-xl bg-current font-xssss fw-700 ls-lg text-white"
 				>
-					Submit
+					{edit ? 'Save' : 'Submit'}
 				</button>
 			</div>
 		</form>
--- src/pages/Request/EachRequestReview.jsx
+++ src/pages/Request/EachRequestReview.jsx
@@ -1,16 +1,68 @@
 import React, { useState } from 'react'
 import moment from 'moment'
 import ReactStars from 'react-rating-stars-component'
+import axios from 'axios'
+import Cookies from 'js-cookie'
+import { notification } from 'antd'
+import { useSelector } from 'react-redux'
+import ReviewForm from '../../components/ReviewForm/ReviewForm'
 
 export default function EachRequestReview({ data }) {
+	const [review, setReview] = useState(data);
 	const [showOptions, setShowOptions] = useState(false)
+	const [deleted, setDeleted] = useState(false)
+	const [edit, setEdit] = useState(false)
+
+	const { user } = useSelector((state) => state?.auth)
+
+	const owner = review?.user?.id === user?.user?.id
+
+	const handleDelete = async () => {
+		try {
+			const res = await axios(
+				process.env.REACT_APP_API_URL + `/reviews/${review?.id}`,
+				{
+					method: 'DELETE',
+					headers: {
+						Authorization: `Bearer ${Cookies.get('token')}`,
+					},
+				}
+			)
+			if (res.data) {
+				setDeleted(true)
+				notification.error({ message: 'Deleted' })
+			}
+		} catch (error) {
+			notification.error({ message: 'Error, please try again' })
+			return Promise.reject(error)
+		}
+	}
+
+	if (deleted) {
+		return null
+	}
+
+	if (edit) {
+		return (
+			<ReviewForm
+				heading={'Edit your review'}
+				request={review?.request}
+				reviewData={review}
+				edit={true}
+				done={e => {
+					setReview(e);
+					setEdit(false)
+				}}
+			/>
+		)
+	}
 
 	return (
 		<article className="card w-100 shadow-xss rounded-xxl border-0 p-4 mb-3">
 			<div className="card-body p-0 d-flex">
 				<figure className="avatar me-3">
 					<img
-						src={data?.user?.avatar_url}
+						src={review?.user?.avatar_url}
 						alt="image"
 						className="shadow-sm rounded-circle w45"
 					/>
@@ -18,86 +70,105 @@ export default function EachRequestReview({ data }) {
 				<h4 className="fw-700 text-grey-900 font-xssss mt-1 w-100">
 					<div className="d-flex justify-content-between">
 						<div>
-							<span>{data?.user?.first_name}</span>
+							<span>{review?.user?.first_name}</span>
 							<ReactStars
-								value={data?.rating}
+								value={review?.rating}
 								edit={false}
 								count={5}
-								size={!data?.review ? 30 : 17}
+								size={!review?.review ? 30 : 17}
 								activeColor="#1da01d"
 								color={'#8e928e'}
 							/>
 						</div>
 						<div>
-							<a
-								onClick={() => setShowOptions(!showOptions)}
-								// className="ms-auto"
-								id="dropdownMenu7"
-								data-toggle="dropdown"
-								aria-haspopup="true"
-								aria-expanded="false"
-							>
-								<i className="ti-more-alt text-grey-900 btn-round-md bg-greylight font-xss"></i>
-							</a>
+							{user && (
+								<a
+									onClick={() => setShowOptions(!showOptions)}
+									// className="ms-auto"
+									id="dropdownMenu7"
+									data-toggle="dropdown"
+									aria-haspopup="true"
+									aria-expanded="false"
+								>
+									<i className="ti-more-alt text-grey-900 btn-round-md bg-greylight font-xss"></i>
+								</a>
+							)}
 						</div>
 					</div>
 
 					<p
 						className={`${
-							!data?.review ? 'mt-0' : 'mt-3'
+							!review?.review ? 'mt-0' : 'mt-3'
 						} text-grey-700 fw-500`}
 						style={{ fontSize: '15px' }}
 					>
 						<span className="d-block font-xssss fw-500 mt-1 lh-3 text-grey-500">
-							{moment(data?.created_at).fromNow()}
+							{moment(review?.created_at).fromNow()}
 						</span>
-						{data?.review}
+						{review?.review}
 					</p>
 				</h4>
 
-				<div
-					className={`dropdown-menu dropdown-menu-end p-4 rounded-xxl border-0 shadow-lg ${
-						showOptions && 'show'
-					}`}
-					aria-labelledby="dropdownMenu6"
-					data-popper-placement="bottom-end"
-					style={{
-						position: 'absolute',
-						inset: '0px auto auto 0px',
-						margin: '0px',
-						transform: 'translate(349px, 73px)',
-						left: '70px',
-					}}
-				>
-					<div className="link card-body p-0 d-flex mb-3">
-						<i className="feather-edit text-grey-500 me-3 font-lg"></i>
-						<h4 className="fw-600 text-grey-900 font-xssss mt-0 me-4">
-							Edit
-							<span className="d-block font-xsssss fw-500 mt-1 lh-3 text-grey-500">
-								Typo? Edit your review
-							</span>
-						</h4>
+				{user && (
+					<div
+						className={`dropdown-menu dropdown-menu-end p-4 rounded-xxl border-0 shadow-lg ${
+							showOptions && 'show'
+						}`}
+						aria-labelledby="dropdownMenu6"
+						data-popper-placement="bottom-end"
+						style={{
+							position: 'absolute',
+							inset: '0px auto auto 0px',
+							margin: '0px',
+							transform: 'translate(349px, 73px)',
+							left: '70px',
+						}}
+					>
+						{owner && (
+							<div
+								className="link card-body p-0 d-flex mb-3"
+								onClick={() => {
+									setShowOptions(false)
+									setEdit(true);
+								}}
+							>
+								<i className="feather-edit text-grey-500 me-3 font-lg"></i>
+								<h4 className="fw-600 text-grey-900 font-xssss mt-0 me-4">
+									Edit
+									<span className="d-block font-xsssss fw-500 mt-1 lh-3 text-grey-500">
+										Typo? Edit your review
+									</span>
+								</h4>
+							</div>
+						)}
+						<div className="link card-body p-0 d-flex">
+							<i className="feather-flag text-grey-500 me-3 font-lg"></i>
+							<h4 className="fw-600 text-grey-900 font-xssss mt-0 me-4">
+								Report
+								<span className="d-block font-xsssss fw-500 mt-1 lh-3 text-grey-500">
+									Broke a rule? Please report this.
+								</span>
+							</h4>
+						</div>
+						{owner && (
+							<>
+								<hr />
+								<div
+									className="link card-body p-0 d-flex mt-2"
+									onClick={handleDelete}
+								>
+									<i className="feather-trash text-grey-500 me-3 font-lg"></i>
+									<h4 className="fw-600 text-grey-900 font-xssss mt-0 me-4">
+										Delete
+										<span className="d-block font-xsssss fw-500 mt-1 lh-3 text-grey-500">
+											Delete your review.
+										</span>
+									</h4>
+								</div>
+							</>
+						)}
 					</div>
-					<div className="link card-body p-0 d-flex">
-						<i className="feather-flag text-grey-500 me-3 font-lg"></i>
-						<h4 className="fw-600 text-grey-900 font-xssss mt-0 me-4">
-							Report
-							<span className="d-block font-xsssss fw-500 mt-1 lh-3 text-grey-500">
-								Broke a rule? Please report this.
-							</span>
-						</h4>
-					</div>
-					<hr />
-					<div className="link card-body p-0 d-flex mt-2">
-						<i className="feather-trash text-grey-500 me-3 font-lg"></i>
-						<h4 className="fw-600 text-grey-900 font-xssss mt-0 me-4">
-							Delete
-							<span className="d-block font-xsssss fw-500 mt-1 lh-3 text-grey-500">
-								Delete your review.
-							</span>
-						</h4>
-					</div>
-				</div>
+				)}
 			</div>
 		</article>
 	)
