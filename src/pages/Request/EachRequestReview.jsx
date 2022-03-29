ab094ebb6e4a1c1394a69af0aae295fec28d1987
emykumba <emy@meetkumba.com>
2022-03-29 14:30:51 +0100

    fixed each review

--- src/pages/Request/EachRequestReview.jsx
+++ src/pages/Request/EachRequestReview.jsx
@@ -62,122 +62,69 @@ export default function EachRequestReview({ data, withRating }) {
 	return (
 		<article className="card w-100 shadow-xss rounded-xxl border-0 p-4 mb-3">
 			<div className="card-body p-0 d-flex">
-				<figure className="avatar me-3">
+				<figure className="avatar me-3 m-0">
 					<img
 						src={review?.user?.avatar_url}
 						alt="image"
 						className="shadow-sm rounded-circle w45"
 					/>
 				</figure>
-				<h4 className="fw-700 text-grey-900 font-xssss mt-1 w-100">
-					<div className="d-flex justify-content-between">
-						<div>
-							<span>{review?.user?.first_name}</span>
-							{withRating ? (
-								<ReactStars
-									value={review?.rating}
-									edit={false}
-									count={5}
-									size={!review?.review ? 30 : 17}
-									activeColor="#1da01d"
-									color={'#8e928e'}
-								/>
-							) : (
-								<div>
-									<small>@{review?.user?.username}</small>
-								</div>
-							)}
-						</div>
-						<div>
-							{user && (
-								<a
-									onClick={() => setShowOptions(!showOptions)}
-									// className="ms-auto"
-									id="dropdownMenu7"
-									data-toggle="dropdown"
-									aria-haspopup="true"
-									aria-expanded="false"
-								>
-									<i className="ti-more-alt text-grey-900 btn-round-md bg-greylight font-xss"></i>
-								</a>
-							)}
-						</div>
-					</div>
-
-					<p
-						className={`${
-							!review?.review ? 'mt-0' : 'mt-2'
-						} text-grey-700 fw-500`}
-						style={{ fontSize: '15px' }}
-					>
-						<span className="d-block font-xssss fw-500 mt-1 lh-3 text-grey-500">
-							{moment(review?.created_at).fromNow()}
-						</span>
-						{review?.review}
-					</p>
+				<h4 className="fw-700 text-grey-900 font-xssss mt-1">
+					{review?.user?.first_name}
+					<span className="d-block font-xssss fw-500 mt-1 lh-3 text-grey-500">
+						@{review?.user?.username}
+					</span>
 				</h4>
-
-				{user && (
-					<div
-						className={`dropdown-menu dropdown-menu-end p-4 rounded-xxl border-0 shadow-lg ${
-							showOptions && 'show'
-						}`}
-						aria-labelledby="dropdownMenu6"
-						data-popper-placement="bottom-end"
-						style={{
-							position: 'absolute',
-							inset: '5px auto auto -94px',
-							margin: '0px',
-							transform: 'translate(349px, 73px)',
-							left: '70px',
-						}}
-					>
-						{owner && (
-							<div
-								className="link card-body p-0 d-flex mb-3"
-								onClick={() => {
-									setShowOptions(false)
-									setEdit(true)
-								}}
-							>
-								<i className="feather-edit text-grey-500 me-3 font-lg"></i>
-								<h4 className="fw-600 text-grey-900 font-xssss mt-0 me-4">
-									Edit
-									<span className="d-block font-xsssss fw-500 mt-1 lh-3 text-grey-500">
-										Typo? Edit your review
-									</span>
-								</h4>
-							</div>
-						)}
-						{/* <div className="link card-body p-0 d-flex">
-							<i className="feather-flag text-grey-500 me-3 font-lg"></i>
-							<h4 className="fw-600 text-grey-900 font-xssss mt-0 me-4">
-								Report
-								<span className="d-block font-xsssss fw-500 mt-1 lh-3 text-grey-500">
-									Broke a rule? Please report this.
-								</span>
-							</h4>
-						</div> */}
-						{owner && (
-							<>
-								<hr />
-								<div
-									className="link card-body p-0 d-flex mt-2"
-									onClick={handleDelete}
-								>
-									<i className="feather-trash text-grey-500 me-3 font-lg"></i>
-									<h4 className="fw-600 text-grey-900 font-xssss mt-0 me-4">
-										Delete
-										<span className="d-block font-xsssss fw-500 mt-1 lh-3 text-grey-500">
-											Delete your review.
-										</span>
-									</h4>
-								</div>
-							</>
-						)}
+				<a
+					href="#"
+					className="ms-auto"
+					id="dropdownMenu6"
+					data-bs-toggle="dropdown"
+					aria-expanded="false"
+				>
+					<i className="ti-more-alt text-grey-900 btn-round-md bg-greylight font-xss"></i>
+				</a>
+				<div
+					className="dropdown-menu dropdown-menu-end p-4 rounded-xxl border-0 shadow-lg"
+					aria-labelledby="dropdownMenu6"
+				>
+					<div className="card-body p-0 d-flex" onClick={() => setEdit(true)}>
+						<i className="feather-edit text-grey-500 me-3 font-lg"></i>
+						<h4 className="fw-600 text-grey-900 font-xssss mt-0 me-4">
+							Edit
+							<span className="d-block font-xsssss fw-500 mt-1 lh-3 text-grey-500">
+								Typo? Edit your review
+							</span>
+						</h4>
 					</div>
-				)}
+					{/* <div className="card-body p-0 d-flex mt-2">
+						<i className="feather-flag text-grey-500 me-3 font-lg"></i>
+						<h4 className="fw-600 text-grey-900 font-xssss mt-0 me-4">
+							Report
+							<span className="d-block font-xsssss fw-500 mt-1 lh-3 text-grey-500">
+								Broke a rule? Please report
+							</span>
+						</h4>
+					</div> */}
+					<hr />
+					<div className="card-body p-0 d-flex mt-2" onClick={handleDelete}>
+						<i className="feather-trash text-grey-500 me-3 font-lg"></i>
+						<h4 className="fw-600 text-grey-900 font-xssss mt-0 me-4">
+							Delete
+							<span className="d-block font-xsssss fw-500 mt-1 lh-3 text-grey-500">
+								Delete your review
+							</span>
+						</h4>
+					</div>
+					
+				</div>
 			</div>
+			<div className="card-body p-0 me-lg-5 ml-5">
+				<p className="fw-500 text-grey-500 lh-26 font-xssss w-100">
+					{review?.review}
+				</p>
+			</div>
+			
 		</article>
 	)
 }
