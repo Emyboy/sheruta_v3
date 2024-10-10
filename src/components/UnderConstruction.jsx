4ae3630c0844c5f604285fb429cce21b100d7680
joevibanks <joevibanks@gmail.com>
2024-10-10 08:37:07 +0100

    site under construction

--- src/components/UnderConstruction.jsx
+++ src/components/UnderConstruction.jsx
@@ -15,8 +15,8 @@ export default function UnderConstruction() {
     return (
         <div style={{ textAlign: "center" }}>
             <img src="https://www.pngitem.com/pimgs/m/624-6243093_construction-building-under-clipart-transparent-png-construction-building.png" />
-            <h1>Site is under maintenance</h1>
-            <h6>Please check back in a view minutes 🥺</h6>
+            <h1>Site is undergoing an upgrade</h1>
+            <h6>We will be back up by the on the 15th 🥺</h6>
             <a target={"_blank"} href="https://www.instagram.com/sheruta_ng/">
                 <button className="btn btn-lg btn-success mt-5">
                     Contact US
